597f05ad8827dab12cd4e4663e6dd2aa4600e2fc
BlackPoowerDev <renedfd20@gmail.com>
2026-04-06 12:43:49 -0300

    atualizacao Spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 const Spinner = () => {
   return (
     <div className="absolute w-full h-full bg-black flex justify-center items-center">
-      <Loader className="mr-2 h-5 w-5 animate-spin" />
+      <Loader className="mr-2 h-5 w-10 animate-spin w-" />
     </div>
   );
 };
